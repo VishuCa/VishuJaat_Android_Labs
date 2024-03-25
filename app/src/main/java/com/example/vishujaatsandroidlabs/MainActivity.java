92a225989637bb388cccf4c86b58c99e366150b6
vishuca <jasleenchhabra5061@gmail.com>
2024-03-25 20:14:49 +0530

    Finished Phone Version of Lab 3

--- app/src/main/java/com/example/vishujaatsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/vishujaatsandroidlabs/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.vishujaatsandroidlabs;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView flagImageView;
+    private Switch switch1; // Corrected variable name
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        flagImageView = findViewById(R.id.flagImageView);
+        switch1 = findViewById(R.id.switch1); // Corrected ID
+
+        switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                flagImageView.startAnimation(rotate);
+            } else {
+                flagImageView.clearAnimation();
+            }
         });
     }
 }
